5dabc0faafbe997850deff614f9dba4db76c8793
Henry Howes <henryhowes@gmail.com>
2014-12-03 13:34:08 -0800

    Refactored rot13 using .encode() to shorten and added testing block

--- Students/HenryHowes/session03/rot13.py
+++ Students/HenryHowes/session03/rot13.py
@@ -1,9 +1,6 @@
 def rot13(text):
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o,' 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
-    for position, letter in enumerate(text):
-        if(letter.isalpha()):
-            text[position] = alphabet[(alphabet.index(letter.lower()) + 13)]
-    return text
+    return text.encode('rot_13')
 
-
-print rot13("test")
+if __name__ == '__main__':
+    assert rot13(u"TeSt") == u"GrFg"
+    print "Encoding successful"
