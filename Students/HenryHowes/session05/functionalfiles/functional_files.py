8b96d634cd10b68ba21df5edb225c91e87051a1e
Henry Howes <henryhowes@gmail.com>
2014-12-13 18:38:56 -0800

    adds final versions of functional_files and functional_files_2
    both versions work as expected, check for acceptable input, and prompt for a filename to strip whitespace from (if none is supplied)
    functional_files uses map() to strip whitespace
    functional_files_2 uses list comprehension

---
+++ Students/HenryHowes/session05/functionalfiles/functional_files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def trim_line(line):
+    return ("{}".format(line.lstrip().rstrip()))
+
+try:
+    filename = sys.argv[1]
+except IndexError:
+    filename = ""
+    while filename[-4:] != u".txt":
+        filename = raw_input(
+            u"Choose a file to remove leading and trailing whitespace from"
+            "(must end with .txt): ")
+
+the_file = open("{}".format(filename))
+trimmed_lines = map(trim_line, the_file)
+the_file.close()
+
+answer = None
+while answer != u"create new file" and answer != u"overwrite":
+    answer = raw_input(u"Choose one: 'create new file' or 'overwrite' ")
+
+if answer == "create new file":
+    outputfilename = ""
+    while outputfilename[-4:] != u".txt":
+        outputfilename = raw_input(
+            u"Choose a new file name (must end with .txt): ")
+else:
+    outputfilename = filename
+
+output_file = open("{}".format(outputfilename), "w")
+for line in trimmed_lines:
+    output_file.write(u"{}\n".format(line))
+
+output_file.close()
