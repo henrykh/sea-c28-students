83c43d14594d2d5900bf892f128f598813f7bcec
Henry Howes <henryhowes@gmail.com>
2014-12-18 14:57:14 -0800

    adds assert statements to lambda_magic

--- Students/HenryHowes/session05/lambda_magic.py
+++ Students/HenryHowes/session05/lambda_magic.py
@@ -8,7 +8,9 @@ def function_builder(n):
 if __name__ == "__main__":
 
     the_list = function_builder(4)
-    print the_list[0](2)
-    print the_list[1](2)
-    for f in the_list:
-        print(f(5))
+    assert the_list[0](2) == 2
+    assert the_list[1](2) == 3
+    for i, f in enumerate(the_list):
+        assert (f(5)) == i+5
+
+    print "All tests passed"
