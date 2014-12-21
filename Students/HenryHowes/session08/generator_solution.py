eafab9ee9e7529baf63d5f792e01d5cdad3cf9d7
Henry Howes <henryhowes@gmail.com>
2014-12-21 10:33:23 -0800

    adds final version of generator_solution with intsum2

---
+++ Students/HenryHowes/session08/generator_solution.py
@@ -0,0 +1,42 @@
+
+import math
+
+
+def intsum(num=0):
+    counter = 0
+    while True:
+        yield num
+        counter += 1
+        num += counter
+
+
+def intsum2(num=0):
+    while True:
+        yield (num * (num + 1)) / 2
+        num += 1
+
+
+def doubler(num=1):
+    while True:
+        yield num
+        num *= 2
+
+
+def fib(num=1):
+    twoback, oneback = 0, 0
+    while True:
+        yield num
+        twoback, oneback = oneback, num
+        num = oneback + twoback
+
+
+def prime(num=2):
+    yield num
+    num += 1
+    while True:
+        for x in range(3, int(math.sqrt(num)+1), 2):
+            if num % x == 0:
+                break
+        else:
+            yield num
+        num += 2
